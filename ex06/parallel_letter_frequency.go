3d7c47edfb9641858ae5c094f7d20466fee8d245
Alexander Kolesnik <alex60070@gmail.com>
2018-07-11 18:00:55 +0300

    ex06: done

---
+++ ex06/parallel_letter_frequency.go
@@ -0,0 +1,50 @@
+package letter 
+
+import "sync"
+
+func add(c rune, letters *map[rune]int, mutex *sync.Mutex){
+	(*mutex).Lock()
+
+	_, ok := (*letters)[c]
+	if !ok{
+		(*letters)[c] = 1
+	}else{
+		(*letters)[c]+=1
+	}
+
+	(*mutex).Unlock()
+}
+
+func Count(text string, letters *map[rune]int, mutex *sync.Mutex, chanel chan bool){
+	for _,c:=range([]rune(text)){
+		add(rune(c), letters, mutex)
+	}
+	chanel<-true
+}
+
+func Frequency(text string) map[rune]int {
+	var letters map[rune]int = make(map[rune]int)
+	var mutex = &sync.Mutex{}
+	chanel := make(chan bool, 3)
+
+	Count(text, &letters, mutex, chanel)
+	return letters
+}
+
+func ConcurrentFrequency(texts []string) map[rune]int {
+	var letters map[rune]int = make(map[rune]int)
+	var mutex = sync.Mutex{}
+
+	l:=len(texts)
+	chanel := make(chan bool, 3)
+
+	for _,str:=range(texts){
+		go Count(str, &letters, &mutex, chanel)
+	}
+
+	for i:=0; i<l;i++{
+		<-chanel
+	}
+	return letters
+}
+
