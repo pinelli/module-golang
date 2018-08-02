94c9a9a1d30dea1f3522d3ea5ff4fd6656cd6747
Alexander Kolesnik <alex60070@gmail.com>
2018-08-02 17:01:12 +0300

    start ex11

---
+++ ex12-kv/kv.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+)
+
+type Store struct {
+	RaftDir  string
+	RaftBind string
+
+	mu sync.Mutex
+	m  map[string]string // The key-value store for the system.
+
+	raft *raft.Raft // The consensus mechanism
+}
+
+func setRaftDir() {
+
+}
+
+func (s *Store) Open(localID string) error {
+	// Setup Raft configuration.
+	config := raft.DefaultConfig()
+	config.LocalID = raft.ServerID(localID)
+
+	// Setup Raft communication.
+	addr, err := net.ResolveTCPAddr("tcp", s.RaftBind)
+	if err != nil {
+		return err
+	}
+	transport, err := raft.NewTCPTransport(s.RaftBind, addr, 3, 10*time.Second, os.Stderr)
+	if err != nil {
+		return err
+	}
+
+	// Create the snapshot store. This allows the Raft to truncate the log.
+	snapshots, err := raft.NewFileSnapshotStore(s.RaftDir, 2, os.Stderr)
+	if err != nil {
+		return fmt.Errorf("file snapshot store: %s", err)
+	}
+
+	// Create the log store and stable store.
+	var logStore raft.LogStore
+	var stableStore raft.StableStore
+	boltDB, err := raftboltdb.NewBoltStore(filepath.Join(s.RaftDir, "raft.db"))
+	if err != nil {
+		return fmt.Errorf("new bolt store: %s", err)
+	}
+	logStore = boltDB
+	stableStore = boltDB
+
+	// Instantiate the Raft systems.
+	ra, err := raft.NewRaft(config, (*fsm)(s), logStore, stableStore, snapshots, transport)
+	if err != nil {
+		return fmt.Errorf("new raft: %s", err)
+	}
+	s.raft = ra
+
+	configuration := raft.Configuration{
+		Servers: []raft.Server{
+			{
+				ID:      config.LocalID,
+				Address: transport.LocalAddr(),
+			},
+		},
+	}
+	ra.BootstrapCluster(configuration)
+
+	return nil
+}
+
+func KeyValue() {
+
+}
+
+func main() {
+		 store := Store{}
+		 store.RaftDir := "~/kv/db"
+		 store.RaftBind := "localhost:9090"
+		 store.Open()
+}
--- ex12-kv/kv_test.go
+++ ex12-kv/kv_test.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestKeyValue(t *testing.T) {
-	test := assert.New(t)
-
+	//test := assert.New(t)
 	// TODO
 }
